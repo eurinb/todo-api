082d72ad66cdfc2e0462433e149197f4e22f41ce
Ingo Brenckmann <Ingo.Brenckmann@gmail.com>
2017-01-04 20:14:38 +0100

    Refactor api with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,13 +21,8 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	todos.forEach(function(todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		};
-	});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -37,7 +33,17 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	
+	// Use _.pick to only pick description and completed
+	var body = _.pick(req.body, 'description', 'completed');
+
+	// Check validity of data
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+		return res.status(400).send();
+	};
+
+	// set body.description to be trimmed value
+	body.description = body.description.trim();
 
 	// add id field to body
 	body.id = todoNextId++;
