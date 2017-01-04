3b046cfff5c9a7c0980ec10bb4af9bb2d5977553
Ingo Brenckmann <Ingo.Brenckmann@gmail.com>
2017-01-04 22:09:51 +0100

    Variable playground area added

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Ingo',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+	
+// 	// // This will not work
+// 	// obj = {
+// 	// 	name: 'Ingo',
+// 	// 	age: 44
+// 	// };
+// 	obj.age = 44;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrade(grades) {
+	grades.push(55);
+	debugger;
+	//grades = [12, 44, 66, 88];
+}
+
+addGrade(grades);
+console.log(grades);
