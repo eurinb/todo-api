be25ba3075effa0ec0c697ca0fcc04de2288a0d3
Ingo Brenckmann <Ingo.Brenckmann@gmail.com>
2017-01-04 19:44:50 +0100

    Added post for todos

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: "Water plants",
-	completed: false
-}, {
-	id: 2,
-	description: "Go to market",
-	completed: false
-}, {
-	id: 3,
-	description: "Read paper",
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -40,6 +33,19 @@ app.get('/todos/:id', function(req, res) {
 	} else {
 		res.send(404).send();
 	};
+});
+
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+
+	// add id field to body
+	body.id = todoNextId++;
+
+	// push body into array
+	todos.push(body);
+
+	res.json(body);
 });
 
 app.listen(PORT, function() {
