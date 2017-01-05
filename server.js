28191730bc420e89395aed753760a8f764c5494c
Ingo Brenckmann <Ingo.Brenckmann@gmail.com>
2017-01-05 13:33:53 +0100

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -59,21 +60,12 @@ app.post('/todos', function(req, res) {
 	// Use _.pick to only pick description and completed
 	var body = _.pick(req.body, 'description', 'completed');
 
-	// Check validity of data
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	};
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON());
+	}, function(e) {
+		res.status(400).json(e);
+	});
 
-	// set body.description to be trimmed value
-	body.description = body.description.trim();
-
-	// add id field to body
-	body.id = todoNextId++;
-
-	// push body into array
-	todos.push(body);
-
-	res.json(body);
 });
 
 // DELETE //todos/:id
@@ -123,6 +115,9 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT);
-});
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT);
+	});
+})
+
